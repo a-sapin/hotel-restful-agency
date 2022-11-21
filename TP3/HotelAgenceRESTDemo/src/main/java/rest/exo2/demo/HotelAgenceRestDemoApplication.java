19e5b283a70f1105fda0932736960ab7df792c31
Lorenzo Puccio <lorenzo.puccio@etu.umontpellier.fr>
2022-11-21 09:43:52 +0100

    Début TP3

---
+++ TP3/HotelAgenceRESTDemo/src/main/java/rest/exo2/demo/HotelAgenceRestDemoApplication.java
@@ -0,0 +1,13 @@
+package rest.exo2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAgenceRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAgenceRestDemoApplication.class, args);
+	}
+
+}
---
+++ TP3/HotelAgenceRESTDemo/src/test/java/rest/exo2/demo/HotelAgenceRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.exo2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAgenceRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
